0321d9f3ffd79598884496c00bd60bee78d65c77
yiyi17 <ielyiyi17@gmail.com>
2024-02-04 18:41:26 +0800

    feat: rn monitor

--- packages/rn/rn-js-monitor.js
+++ packages/rn/rn-js-monitor.js
@@ -1,120 +1,152 @@
+import { addEventListener } from '@react-native-community/netinfo';
+import { Platform } from 'react-native';
+
+// 采集信息
+// 设备机型
+// 设备操作系统平台
+// 设备操作系统版本
+// 网络条件
+// 客户端版本号
+// React Native Bundle版本号
+
+const { OS, Version, isPad } = Platform;
+
+const netInfo = { type: '', details: {} };
+// Subscribe
+const unsubscribe = addEventListener((state) => {
+  netInfo.type = state.type;
+  netInfo.details = state.details;
+});
+// Unsubscribe
+unsubscribe();
+
+const device = {
+  OS,
+  Version,
+  isPad,
+};
+
+// 客户端传进来
+const app = {
+  version: '',
+  bundleVersion: '',
+};
+
+console.log(device, netInfo, app);
 
 function errorMonitor() {
-  global.ErrorUtils.setGlobalHandler(error => {
+  global.ErrorUtils.setGlobalHandler((error) => {
     console.log('ErrorUtils发现了语法错误，避免了崩溃，具体报错信息：');
-    console.log(error.name, error.message, [{ text: 'OK' }]); 
+    console.log(error.name, error.message, [{ text: 'OK' }]);
   }, true);
 }
 function apiMonitor() {
-  const TIME_LIMIT = 3000
-  const originFetch = fetch
+  const TIME_LIMIT = 3000;
+  const originFetch = fetch;
   Object.defineProperty(global, 'fetch', {
     configurable: true,
     enumerable: true,
     get() {
-      const starTime = Date.now()
+      const starTime = Date.now();
       return (url, options) => {
         return new Promise((resolve, reject) => {
           originFetch(url, options)
-          .then(res => {
-            const endTime = Date.now()
-            const spendTime = endTime - starTime
-            const isOK = (res.status >= 200 && res.status < 300) || res.status === 304;
-            const cloneRes = res.clone();
-            let reqType
-            if (spendTime >= TIME_LIMIT && isOK) {
-              reqType = 'reqPerf';
-            } else if (!isOK) {
-              reqType = 'reqError';
-            }
+            .then((res) => {
+              const endTime = Date.now();
+              const spendTime = endTime - starTime;
+              const isOK = (res.status >= 200 && res.status < 300) || res.status === 304;
+              const cloneRes = res.clone();
+              let reqType;
+              if (spendTime >= TIME_LIMIT && isOK) {
+                reqType = 'reqPerf';
+              } else if (!isOK) {
+                reqType = 'reqError';
+              }
 
-            console.log(`%c接口${url}请求耗时：${spendTime}ms, ${res.status},返回值 ${JSON.stringify(cloneRes)}`, 'color: blue');
-            resolve(res)
-          }).catch(err => {
-            reject(err);
-          })
-        })
-      }
+              console.log(`%c接口${url}请求耗时：${spendTime}ms, ${res.status}`, 'color: blue');
+              resolve(res);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        });
+      };
     },
-    set() {
-
-    }
-  })
+    set() {},
+  });
 }
 
 function calculate(children) {
-  let count = 0
+  let count = 0;
   function fn(children) {
-    for(let i = 0; i < children.length; i++) {
+    for (let i = 0; i < children.length; i++) {
       // 如果是数字
-      if(children[i]._children.length){
-        if(typeof children[i]._children[0] === 'number') {
-          count++
-        }else {
-          fn(children[i]._children)
+      if (children[i]._children.length) {
+        if (typeof children[i]._children[0] === 'number') {
+          count++;
+        } else {
+          fn(children[i]._children);
         }
       }
     }
- }
- if(children.length) {
-    fn(children)
- }
- console.log(children.length, count);
- return children.length === count
+  }
+  if (children.length) {
+    fn(children);
+  }
+  return children.length === count;
 }
 
 function lcpMonitor(refs) {
-  if(!refs || !refs[0]) return
-  const startTime = Date.now()
-  let start
+  if (!refs || !refs[0]) return;
+  const startTime = Date.now();
+  let start;
   function fn() {
     const stop = requestAnimationFrame((timestamp) => {
       if (start === undefined) {
         start = timestamp;
       }
       const elapsed = timestamp - start;
-      if(elapsed > 10000) {
-        cancelAnimationFrame(stop)
+      if (elapsed > 10000) {
+        cancelAnimationFrame(stop);
       }
-      if(calculate(refs[0]._children)) {
-        console.log(`%c lcp: ${Date.now()-startTime + elapsed} ms`, 'color: red');
-        cancelAnimationFrame(stop)
-      }else {
-        fn()
+      if (calculate(refs[0]._children)) {
+        console.log(`%c lcp: ${Date.now() - startTime + elapsed} ms`, 'color: red');
+        cancelAnimationFrame(stop);
+      } else {
+        fn();
       }
-    })
+    });
   }
-  fn()
+  fn();
 }
 
-function whiteMonitor(refs){
-  let time = 0
-  if(!refs || !refs[0]) return
+function whiteMonitor(refs) {
+  let time = 0;
+  if (!refs || !refs[0]) return;
   const timer = setInterval(() => {
-    time++
-    if(calculate(refs[0]._children)) {
-      clearInterval(timer)
+    time++;
+    if (calculate(refs[0]._children)) {
+      clearInterval(timer);
       console.log('%c页面渲染完成，未发生白屏', 'color: green');
-    }else if(time > 10) {
-      clearInterval(timer)
+    } else if (time > 10) {
+      clearInterval(timer);
       console.log('%c页面渲染完成，发生白屏', 'color: red');
     }
-  },2000)
+  }, 2000);
 }
 
-export default function rnjsMonitor(options) {
-  const {error, lcp, white, api} = options
-  if(error) {
-    errorMonitor()
+export default async function rnjsMonitor(options) {
+  const { error, lcp, white, api } = options;
+  if (error) {
+    errorMonitor();
   }
-  if(api) {
-    apiMonitor()
+  if (api) {
+    apiMonitor();
   }
-  if(lcp && lcp.open) {
-    lcpMonitor(lcp.refs)
+  if (lcp && lcp.open) {
+    lcpMonitor(lcp.refs);
   }
-  if(white && white.open){
-    whiteMonitor(white.refs)
+  if (white && white.open) {
+    whiteMonitor(white.refs);
   }
 }
-
